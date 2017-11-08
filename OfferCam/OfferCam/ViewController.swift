f00dd2087c6aaa3a488b00f9c1619186c63098e0
Suman Kumar Jha <17616710+jhasuman@users.noreply.github.com>
2017-11-08 13:20:54 +0530

    Add files via upload

---
+++ OfferCam/OfferCam/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  OfferCam
+//
+//  Created by Aqeel on 11/7/17.
+//  Copyright © 2017 Aqeel. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
